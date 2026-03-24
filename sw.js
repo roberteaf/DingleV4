30ead803803659dd6ad50e3e43c3ccbaefeaf181
roberteaf <robertcosofret@yahoo.com>
2026-03-24 10:19:31 +0000

    Update sw.js
    Signed-off-by: roberteaf <robertcosofret@yahoo.com>

--- sw.js
+++ sw.js
@@ -1,19 +1,21 @@
-self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-    if (url.pathname.includes('/service/')) {
-        const targetUrl = decodeURIComponent(url.pathname.split('/service/')[1]);
-        
-        /* Using a faster 2026 Mirror for Google/Apps */
-        const proxyUrl = `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(targetUrl)}`;
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 
-        event.respondWith(
+self.addEventListener('fetch', e => {
+    const url = new URL(e.request.url);
+    if (url.pathname.includes('/service/')) {
+        const target = decodeURIComponent(url.pathname.split('/service/')[1]);
+        const proxyUrl = 'https://api.codetabs.com/v1/proxy?quest=' + encodeURIComponent(target);
+        e.respondWith(
             fetch(proxyUrl)
-                .then(res => res.text())
-                .then(data => {
-                    return new Response(data, {
-                        headers: { 'Content-Type': 'text/html' }
-                    });
-                })
+                .then(r => r.text())
+                .then(html => new Response(html, {
+                    headers: { 'Content-Type': 'text/html; charset=utf-8' }
+                }))
+                .catch(() => new Response('<h1 style="color:red">Failed to load</h1>', {
+                    status: 502,
+                    headers: { 'Content-Type': 'text/html' }
+                }))
         );
     }
 });
