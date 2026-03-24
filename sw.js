700d35445656661df2bfbfc483b8c14f333a56c3
roberteaf <robertcosofret@yahoo.com>
2026-03-24 08:51:08 +0000

    Update sw.js
    Signed-off-by: roberteaf <robertcosofret@yahoo.com>

--- sw.js
+++ sw.js
@@ -1,31 +1,19 @@
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim()); // Force the proxy to start working IMMEDIATELY
-});
-
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
-
-    /* Detection for /service/ regardless of the GitHub folder name */
     if (url.pathname.includes('/service/')) {
-        const parts = url.pathname.split('/service/');
-        const targetUrl = decodeURIComponent(parts[1]);
-
-        /* AllOrigins Engine */
-        const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(targetUrl)}`;
+        const targetUrl = decodeURIComponent(url.pathname.split('/service/')[1]);
+        
+        /* Using a faster 2026 Mirror for Google/Apps */
+        const proxyUrl = `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(targetUrl)}`;
 
         event.respondWith(
             fetch(proxyUrl)
-                .then(res => res.json())
+                .then(res => res.text())
                 .then(data => {
-                    return new Response(data.contents, {
+                    return new Response(data, {
                         headers: { 'Content-Type': 'text/html' }
                     });
                 })
-                .catch(err => new Response("Proxy Error: " + err, { status: 500 }))
         );
     }
 });
